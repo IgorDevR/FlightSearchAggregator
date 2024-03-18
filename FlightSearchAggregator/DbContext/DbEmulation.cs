a3f577c9887044df862b953603b5faa1ea5fc3cf
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-03-19 00:04:05 +0700

    Add Booking, DbEmulation
    -Added Booking.cs model to save in case of successful booking
    -Added DbEmulation.cs a dictionary that emulates working with a database

---
+++ FlightSearchAggregator/DbContext/DbEmulation.cs
@@ -0,0 +1,9 @@
+﻿using FlightSearchAggregator.Models;
+using FlightSearchAggregator.Services.Bookings;
+
+namespace FlightSearchAggregator.DbContext;
+
+public static class DbEmulation
+{
+    public static Dictionary <Guid, Booking> Bookings  = new Dictionary<Guid, Booking> ();
+}
