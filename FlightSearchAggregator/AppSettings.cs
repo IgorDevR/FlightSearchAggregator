0d77d520274c2c4ba58291dacf01789db3eecd47
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-03-18 16:31:15 +0700

    Add AppSettings
    -Added AppSettings.cs

---
+++ FlightSearchAggregator/AppSettings.cs
@@ -0,0 +1,10 @@
+namespace FlightSearchAggregator
+{
+    public class AppSettings
+    {
+        public string FlyQuestBaseUrl { get; set; }
+
+        public string SkyTrailsBaseUrl { get; set; }
+        public Guid ServiceId { get; set; }
+    }
+}
