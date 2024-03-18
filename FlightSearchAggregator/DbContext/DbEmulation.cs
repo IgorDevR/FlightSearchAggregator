2cfa9f37b763ac3034f656f8bedf574f93e0e14d
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-03-19 06:08:20 +0700

    Change FlightSearchAggregator,DbEmulation
    -Added FlightSearchAggregator new packages JwtBearer, SwashbuckleFilters
    -Added DbEmulation Users

--- FlightSearchAggregator/DbContext/DbEmulation.cs
+++ FlightSearchAggregator/DbContext/DbEmulation.cs
@@ -1,4 +1,5 @@
-﻿using FlightSearchAggregator.Models;
+﻿using FlightSearchAggregator.Auth;
+using FlightSearchAggregator.Models;
 using FlightSearchAggregator.Services.Bookings;
 
 namespace FlightSearchAggregator.DbContext;
@@ -6,4 +7,5 @@ namespace FlightSearchAggregator.DbContext;
 public static class DbEmulation
 {
     public static Dictionary <Guid, Booking> Bookings  = new Dictionary<Guid, Booking> ();
+    public static Dictionary<string, User> Users = new Dictionary<string, User>();
 }
