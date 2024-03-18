9a6e7d348e1fa9246ae26ee7915dcd6842184f10
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-03-19 00:15:27 +0700

    Change BookingRequest
    -Changed BookingRequest add override ToString

--- FlightSearchAggregator/Dtos/BookingRequest.cs
+++ FlightSearchAggregator/Dtos/BookingRequest.cs
@@ -11,9 +11,21 @@ public class BookingRequest
     [Helpers.Validator(ErrorMessage = "PassengerName must not be null or empty")]
     public string PassengerName { get; set; }
 
+    [Helpers.Validator(ErrorMessage = "Phone must not be null or empty")]
+    public string Phone { get; set; }
+
     [Helpers.Validator(ErrorMessage = "Email must not be null or empty")]
     public string Email { get; set; }
 
     [Helpers.Validator(ErrorMessage = "Email must not be null or empty")]
     public long Seat { get; set; }
+
+    public override string ToString()
+    {
+        {
+            return
+                $"FlightBookingDto: FlightId: {FlightId}, DataProvider: {DataProvider}, " +
+                $"PassengerName: {PassengerName}, Phone: {Phone}, Email: {Email}, Seat: {Seat}";
+        }
+    }
 }
