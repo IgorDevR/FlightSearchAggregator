02e0dfdb3fe2b0059666fadf551705ae8deb7985
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-03-19 00:10:19 +0700

    Add helper classes
    -Added HttpService.cs is a general class for requests
    -Added LoggerExtensions.cs to easily get method name

---
+++ FlightSearchAggregator/Helpers/HttpService.cs
@@ -0,0 +1,46 @@
+﻿namespace FlightSearchAggregator.Helpers;
+
+public class HttpService
+{
+
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly ILogger<HttpService> _logger;
+
+    public HttpService(IHttpClientFactory client, ILogger<HttpService> logger)
+    {
+        _clientFactory = client;
+        _logger = logger;
+    }
+
+    public async Task<T?> ExecuteGetRequest<T>( string clientName, string uri)
+    {
+        try
+        {
+            var client = _clientFactory.CreateClient(clientName);
+            var response = await client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<T>();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, $"GET request to {uri} failed.");
+            throw;
+        }
+    }
+
+    public async Task<TDestination?> ExecutePostRequest<TSource, TDestination>(string clientName, string uri, TSource content)
+    {
+        try
+        {
+            var client = _clientFactory.CreateClient(clientName);
+            var response = await client.PostAsJsonAsync(uri, content);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<TDestination>();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, $"POST request to {uri} with content {typeof(TSource).Name} failed.");
+            throw;
+        }
+    }
+}
