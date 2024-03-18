f04148b29a92b7d4443d2c11f9367a7c833e9cac
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-03-18 23:54:11 +0700

    Added IBookingService
    -Add IBookingService.cs - interface with methods for booking

---
+++ FlightSearchAggregator/Services/Bookings/IBookingService.cs
@@ -0,0 +1,10 @@
+﻿using FlightSearchAggregator.Dtos;
+
+namespace FlightSearchAggregator.Services.Bookings;
+
+public interface IBookingService
+{
+    Task<BookingDetailDto?> SubmitFlightBooking(BookingRequest bookingRequest);
+
+    Task<BookingDetailDto?> GetBookingDetail(Guid id);
+}
