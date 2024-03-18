0187979f28e5d005c7a96557a45a9fbe50ad9507
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-03-19 06:01:02 +0700

    Add AuthController, AuthService and models
    -Added AuthService, AuthController
    -Added LoginModel, RegisterModel, User

---
+++ FlightSearchAggregator/Auth/AuthController .cs
@@ -0,0 +1,44 @@
+﻿using FlightSearchAggregator.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightSearchAggregator.Auth;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly AuthService _authService;
+    private readonly ILogger _logger;
+
+    public AuthController(AuthService authService, ILogger<AuthController> logger)
+    {
+        _authService = authService;
+        _logger = logger;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register([FromBody] RegisterModel model)
+    {
+        _logger.LogInformationWithMethod($"RegisterModel: {model}");
+        var result = _authService.Register(model.FullName, model.Email, model.Password);
+        if (!result)
+        {
+            return BadRequest("User already exists.");
+        }
+
+        return Ok("User registered successfully.");
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginModel model)
+    {
+        _logger.LogInformationWithMethod($"LoginModel: {model}");
+        var token = _authService.Login(model.Email, model.Password);
+        if (token == null)
+        {
+            return Unauthorized("Invalid email or password.");
+        }
+
+        return Ok(new { Token = token });
+    }
+}
---
+++ FlightSearchAggregator/Auth/User.cs
@@ -0,0 +1,9 @@
+﻿namespace FlightSearchAggregator.Auth;
+
+public class User
+{
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; set; }
+    public string TokenForExample { get; set; }
+}
