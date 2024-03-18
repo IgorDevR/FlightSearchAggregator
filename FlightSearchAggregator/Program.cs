1c124f0e4aee59c313f875ff136591e442e1e0c4
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-03-18 16:38:49 +0700

    Change Program
    -Refactoring
    -Added services

--- FlightSearchAggregator/Program.cs
+++ FlightSearchAggregator/Program.cs
@@ -1,25 +1,74 @@
+using FlightSearchAggregator;
+using FlightSearchAggregator.Services;
+using FlightSearchAggregator.Services.Bookings;
+using FlightSearchAggregator.Services.Providers;
+using Microsoft.OpenApi.Models;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ConfigureAppSettings(builder.Services, builder.Configuration);
+ConfigureServices(builder.Services);
+ConfigureLogging(builder.Host);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+ConfigureApp(app);
 
 app.Run();
+
+void ConfigureAppSettings(IServiceCollection services, IConfiguration configuration)
+{
+    services.Configure<AppSettings>(configuration);
+}
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllers().AddJsonOptions(opts =>
+    {
+        opts.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
+
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+        c.UseInlineDefinitionsForEnums();
+    });
+
+    services.AddMemoryCache();
+    services.AddAutoMapper(typeof(Program));
+    services.AddLogging();
+    services.AddScoped<FlightService>();
+    services.AddScoped<IFlyQuestService, FlyQuestService>();
+    services.AddScoped<ISkyTrailsService, SkyTrailsService>();
+ 
+}
+
+void ConfigureLogging(IHostBuilder hostBuilder)
+{
+    hostBuilder.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .WriteTo.Console()
+        .WriteTo.File("logs/myapp.log", rollingInterval: RollingInterval.Day,
+            fileSizeLimitBytes: 10_485_760,
+            rollOnFileSizeLimit: true,
+            retainedFileCountLimit: null,
+            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}"));
+}
+
+void ConfigureApp(WebApplication app)
+{
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+}
