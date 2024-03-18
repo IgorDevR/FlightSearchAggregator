12388661370528996314dc95424b5d8e484a10a5
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-03-18 23:59:58 +0700

    Added BookingFlightsController
    -Add BookingFlightsController.cs for receiving and booking

---
+++ FlightSearchAggregator/Controllers/BookingFlightsController.cs
@@ -0,0 +1,50 @@
+﻿using FlightSearchAggregator.Dtos;
+using FlightSearchAggregator.Helpers;
+using FlightSearchAggregator.Models;
+using FlightSearchAggregator.Services.Bookings;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightSearchAggregator.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BookingFlightsController : ControllerBase
+{
+    private readonly BookFlightService _bookFlightService;
+    private readonly ILogger _logger;
+
+    public BookingFlightsController(BookFlightService bookFlightService, ILogger<FlightsController> logger)
+    {
+        _bookFlightService = bookFlightService;
+        _logger = logger;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetBookingDetail(Guid id)
+    {
+        _logger.LogInformationWithMethod($"Guid: {id}");
+        var bookingDetail = await _bookFlightService.GetBookingDetail(id);
+        if (bookingDetail == null)
+        {
+            return NotFound($"Booking with id: {id} not found");
+        }
+
+        return Ok(bookingDetail);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> BookFlight([FromBody] BookingRequest bookingRequest)
+    {
+        _logger.LogInformationWithMethod($"BookingRequest: {bookingRequest}");
+
+        var dataProvider = EnumExtensions.ParseEnum(bookingRequest.DataProvider, FlightDataProvider.Unknown);
+        if (dataProvider == FlightDataProvider.Unknown)
+        {
+            return NotFound($"Flight data provider with name: {bookingRequest.DataProvider} not found");
+        }
+
+        var bookingDetail = await _bookFlightService.BookFlight(bookingRequest, dataProvider);
+
+        return Ok(bookingDetail);
+    }
+}
