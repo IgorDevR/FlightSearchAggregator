01c0dde0193627638a2370674ad275d58b34f92b
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-03-18 23:58:58 +0700

    Added BookFlightService, BookingServiceFactory
    -Add BookingServiceFactory.cs - factory to create instances of
    a specific provider
    -Add BookFlightService.cs - booking service using BookingServiceFactory

---
+++ FlightSearchAggregator/Services/Bookings/BookFlightService.cs
@@ -0,0 +1,37 @@
+﻿using FlightSearchAggregator.DbContext;
+using FlightSearchAggregator.Dtos;
+using FlightSearchAggregator.Models;
+
+namespace FlightSearchAggregator.Services.Bookings;
+
+public class BookFlightService
+{
+    private readonly ILogger _logger;
+    private readonly BookingServiceFactory _bookingServiceFactory;
+
+    public BookFlightService(BookingServiceFactory bookingServiceFactory, ILogger<BookFlightService> logger)
+    {
+        _bookingServiceFactory = bookingServiceFactory;
+        _logger = logger;
+    }
+
+    public async Task<BookingDetailDto> BookFlight(BookingRequest bookingRequest, FlightDataProvider dataProvider)
+    {
+        var bookingService = _bookingServiceFactory.GetBookingService(dataProvider);
+        var bookingDetail = await bookingService.SubmitFlightBooking(bookingRequest);
+        return bookingDetail;
+    }
+
+    public async Task<BookingDetailDto?> GetBookingDetail(Guid id)
+    {
+        if (!DbEmulation.Bookings.TryGetValue(id, out var booking))
+        {
+            return null;
+        }
+
+        var bookingService = _bookingServiceFactory.GetBookingService(booking.DataProvider);
+        var bookingDetailDto = await bookingService.GetBookingDetail(booking.ProviderBookingId);
+
+        return bookingDetailDto;
+    }
+}
---
+++ FlightSearchAggregator/Services/Bookings/BookingServiceFactory.cs
@@ -0,0 +1,34 @@
+﻿using FlightSearchAggregator.Models;
+
+namespace FlightSearchAggregator.Services.Bookings;
+
+public class BookingServiceFactory
+{
+    private readonly Dictionary<FlightDataProvider, IBookingService> _bookingServices;
+    private readonly ILogger<BookingServiceFactory> _logger;
+
+    public BookingServiceFactory(
+        IBookingService flyQuestService,
+        IBookingService skyTrailsService,
+        ILogger<BookingServiceFactory> logger)
+    {
+        _bookingServices = new Dictionary<FlightDataProvider, IBookingService>
+        {
+            { FlightDataProvider.FlyQuest, flyQuestService },
+            { FlightDataProvider.SkyTrails, skyTrailsService }
+        };
+        _logger = logger;
+    }
+
+    public IBookingService GetBookingService(FlightDataProvider dataProvider)
+    {
+        if (!_bookingServices.TryGetValue(dataProvider, out var bookingService))
+        {
+            var errorMsg = $"Unsupported flight data provider: {dataProvider}.";
+            _logger.LogError(errorMsg);
+            throw new NotSupportedException(errorMsg);
+        }
+
+        return bookingService;
+    }
+}
