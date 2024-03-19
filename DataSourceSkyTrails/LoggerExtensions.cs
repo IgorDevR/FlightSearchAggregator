046ac7361c67ca4d69d05764c70a8b49275e7bfb
Igor <47169805+IgorDevR@users.noreply.github.com>
2024-03-20 02:25:45 +0700

    Add LoggerExtensions to DataSources

---
+++ DataSourceSkyTrails/LoggerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DataSourceSkyTrails;
+
+public static class LoggerExtensions
+{
+    public static void LogInformationWithMethod(this ILogger logger, string message, [CallerMemberName] string methodName = "")
+    {
+        logger.LogInformation($"{methodName}. {message}");
+    }
+
+    public static void LogErrorWithMethod(this ILogger logger, Exception ex, string message, [CallerMemberName] string methodName = "")
+    {
+        logger.LogError(ex, $"{methodName}. {message}");
+    }
+
+}
